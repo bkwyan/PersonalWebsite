0fef19f3b4dd4bb2332438f50c1184bdc118d2aa
Brandon Yan <byan54@uwo.ca>
2020-10-29 00:25:09 -0400

    Website setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div className='header'>
+            <p>Brandon Kin Wing Yan</p>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return(
+        <div className='navbar'>
+            <a>Brandon Yan</a>
+            <a>Experience</a>
+            <a>Projects</a>
+        </div>
+    );
+}
+
+export default NavBar;
